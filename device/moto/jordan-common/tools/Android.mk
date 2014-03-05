d700d935e62087b8c3070d1a995669e8bbde258e
Philip <noemail>
2014-03-05 22:49:12 +0000

    directory fixes

--- device/moto/jordan-common/tools/Android.mk
+++ device/moto/jordan-common/tools/Android.mk
@@ -50,6 +50,6 @@ LOCAL_MODULE := 2nd-init
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_LIBRARIES += libc
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/bootmenu/binary
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/bootstrap/binary
 
 include $(BUILD_EXECUTABLE)
